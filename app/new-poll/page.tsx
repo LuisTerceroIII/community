5ee7391db8798679da1610f6488abb2518f05931
Luis Espinoza <luis.espinoza.nav@live.com>
2024-05-26 17:07:05 -0300

    add flowbite

--- app/new-poll/page.tsx
+++ app/new-poll/page.tsx
@@ -3,6 +3,7 @@
 import { StepsBar } from "@/components/modules/poll/creation/StepsBar";
 import { StepsCreationSection } from "@/components/modules/poll/creation/StepsCreationSection";
 import { StepsTitle } from "@/components/modules/poll/creation/StepsTitle";
+import { Header } from "@/components/organisms/header";
 import { usePollUIStore } from "@/model/ui-store/polls/polls-ui-model";
 
 export default function Page() {
@@ -10,7 +11,8 @@ export default function Page() {
 	const { currentStep, getActionStep, getStepsCount } = usePollUIStore();
 
     return (
-        <main className="flex align-center min-h-screen p-12 bg-slate-50 flex-col w-full">
+        <main className="flex align-center min-h-screen bg-slate-50 flex-col w-full">
+            <Header />
             <h1 className="text-4xl text-slate-800 text-center">Create new poll</h1>
             <StepsTitle classes={["text-center", "pt-4"]} />
             <StepsBar classes={["text-center", "mt-4", "self-center"]} />
